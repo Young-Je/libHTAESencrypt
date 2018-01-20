1ccc0ff2e373cdf44d76f6a3e17bcd443c4e2869
lev012 <lev012@icloud.com>
2018-01-21 00:27:17 +0800

    Initial Commit

---
+++ libHTAESencrypt/libHTAESencrypt.h
@@ -0,0 +1,13 @@
+//
+//  libHTAESencrypt.h
+//  libHTAESencrypt
+//
+//  Created by yangzhexu on 21/01/2018.
+//  Copyright © 2018 yangzhexuxjtu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface libHTAESencrypt : NSObject
+
+@end
---
+++ libHTAESencrypt/libHTAESencrypt.m
@@ -0,0 +1,13 @@
+//
+//  libHTAESencrypt.m
+//  libHTAESencrypt
+//
+//  Created by yangzhexu on 21/01/2018.
+//  Copyright © 2018 yangzhexuxjtu. All rights reserved.
+//
+
+#import "libHTAESencrypt.h"
+
+@implementation libHTAESencrypt
+
+@end
